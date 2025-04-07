0687666093e38b2dd7f6a11b4be55e53b066cc32
Zack Hanni <zack.hannI@gmail.com>
2025-04-07 14:47:53 -0400

    created duckduckgo search tool

---
+++ tools.py
@@ -0,0 +1,11 @@
+from langchain_community.tools import WikipediaQueryRun, DuckDuckGoSearchRun
+from langchain_community.utilities import WikipediaAPIWrapper
+from langchain.tools import Tool
+from datetime import datetime
+
+search = DuckDuckGoSearchRun()
+search_tool = Tool(
+    name="search",
+    func=search.run,
+    description="Search the web for information"
+)
